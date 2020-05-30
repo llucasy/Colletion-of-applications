d230f0d441ead68b70680e8afb5cd65db48dc35e
llucasy <llucasy@hotmail.com>
2020-05-29 21:00:37 -0300

    Border - Added result

--- BorderRadius/script.js
+++ BorderRadius/script.js
@@ -3,6 +3,11 @@ let values = document.querySelectorAll('input[type="range"]')
 const form = document.querySelector('.form')
 
 form.addEventListener('input', () => {
-    document.querySelector('.borderRadius').style.borderRadius = `${values[0].value}% ${values[1].value}%`
+
+    let strBorderRadius = `${values[0].value}% ${values[1].value}% ${values[2].value}% ${values[3].value}% / ${values[4].value}% ${values[5].value}% ${values[6].value}% ${values[7].value}%`
+
+    document.querySelector('.borderRadius').style.borderRadius = strBorderRadius
+
+    document.querySelector('.result').innerHTML = `border-radius: ${strBorderRadius}`
 })
 
