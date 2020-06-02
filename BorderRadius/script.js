7774b9222de5c6784008a0d3cfc773131dec3fe1
llucasy <llucasy@hotmail.com>
2020-06-02 01:23:38 -0300

    function copyToClipboard + README Border Radius

--- BorderRadius/script.js
+++ BorderRadius/script.js
@@ -2,12 +2,27 @@ let values = document.querySelectorAll('input[type="range"]')
 
 const form = document.querySelector('.form')
 
+const copyToClipboard = copy => {
+    let inputCopy = document.createElement("input")
+    inputCopy.value = copy
+
+    document.body.appendChild(inputCopy)
+
+    inputCopy.select()
+    inputCopy.setSelectionRange(0, 99999)
+    document.execCommand('copy')
+
+    document.body.removeChild(inputCopy)
+}
+
 form.addEventListener('input', () => {
 
     let strBorderRadius = `${values[0].value}% ${values[1].value}% ${values[2].value}% ${values[3].value}% / ${values[4].value}% ${values[5].value}% ${values[6].value}% ${values[7].value}%`
 
     document.querySelector('.borderRadius').style.borderRadius = strBorderRadius
 
-    document.querySelector('.result').innerHTML = `border-radius: ${strBorderRadius};`
+    let strCopy = `border-radius: ${strBorderRadius};`
+    document.querySelector('.result').innerHTML = strCopy
+    copyToClipboard(strCopy)
 })
 
--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Below is the list of apps separated by categories:
 ## Beginners:
 
 1. [Bin2Dec](https://llucasy.github.io/Colletion-of-applications/Bin2Dec/ "Bin2Dec")
+1. [Border Radius Previewer](https://llucasy.github.io/Colletion-of-applications/BorderRadius/ "Border Radius Previewer")
