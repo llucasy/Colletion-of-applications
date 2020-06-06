1560646ed5681916a7940d1b8403dc5136d24cf5
llucasy <llucasy@hotmail.com>
2020-06-06 19:31:31 -0300

    Finished palindrome

--- Palindrome/script.js
+++ Palindrome/script.js
@@ -5,26 +5,26 @@ button.addEventListener('click', () => {
 
     const qtNumber = number.value.length
 
-    if (qtNumber > 4 | qtNumber < 2 | isNaN(number.value) == true) {
-        alert("Please enter 2 to 4 digits and numbers")
+    if (qtNumber > 10 | qtNumber < 2 | isNaN(number.value) == true | number.value < 10) {
+        alert("Please enter 2 to 4 digits and numbers larger than 9")
     } else {
-        arr = String(number.value).split("")
-        console.log(arr)
+
+        function reverse(nums) {
+            let reverse = ''
+            for (let i = String(nums).length - 1; i >= 0; i--) {
+                reverse += String(nums)[i]
+            }
+            return reverse;
+        }
+
+        function palindrome(num) {
+            for (let i = num + 1; ; i++) {
+                if (String(i) === reverse(i))
+                    return i
+            }
+        }
+
+        alert(palindrome(Number(number.value)))
     }
-    
-    
 
-    // if (qtNumber == 2) {
-    //     if (number.value < 11) {alert("Favor digitar um número maior que 10").break}
-    //     if (arr[0] === arr[1]) { 
-    //         document.querySelector('#contar').innerHTML = "Esse número já é um palíndromo"
-    //     } else {
-    //         numero = Number(arr[0] + arr[1])
-    //         while (arr[0] != arr[1]) {
-    //             numero -= 1
-                
-    //         }
-    //     }
-    // }
-
-} )
+})
--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Below is the list of apps separated by categories:
 
 1. [Bin2Dec](https://llucasy.github.io/Colletion-of-applications/Bin2Dec/ "Bin2Dec")
 1. [Border Radius Previewer](https://llucasy.github.io/Colletion-of-applications/BorderRadius/ "Border Radius Previewer")
+1. [Palindrome](https://llucasy.github.io/Colletion-of-applications/Palindrome/ "Next smallest palindrome")
